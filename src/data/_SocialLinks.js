9ca89cdbfa85623ec7c7ce7a9af3e08edad3b807
Panda <90091858+PandaSLO@users.noreply.github.com>
2022-07-12 23:00:14 +0200

    Update _SocialLinks.js

--- src/data/_SocialLinks.js
+++ src/data/_SocialLinks.js
@@ -3,24 +3,14 @@ import { faGithub, faLinkedin, faPadlet } from "@fortawesome/free-brands-svg-ico
 
 const socialLinks = [
   {
-    faIcon: faLinkedin,
-    alt: "Linkedin",
-    url: "https://www.linkedin.com/in/evanitsg/",
-  },
-  {
     faIcon: faGithub,
     alt: "GitHub",
-    url: "https://github.com/digipie",
-  },
-  {
-    faIcon: faPadlet,
-    alt: "Padlet",
-    url: "https://padlet.com/about/us/wish/1072760846",
+    url: "https://github.com/PandaSLO",
   },
   {
     faIcon: faPaperPlane,
     alt: "Email",
-    url: "mailto:evan.it.sg@gmail.com",
+    url: "mailto:info@pandaslo.tk",
   },
 ];
 

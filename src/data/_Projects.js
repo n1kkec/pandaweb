52f7f90602cd786a39e0fb55436b2463c7599a17
Panda <90091858+PandaSLO@users.noreply.github.com>
2022-08-09 21:30:11 +0200

    empty projects table

--- src/data/_Projects.js
+++ src/data/_Projects.js
@@ -1,50 +1,5 @@
 import React from "react";
 
-const projects = [
-  {
-    category: "Project",
-    title: "nodejs-mysql-cloudformation",
-    slug: "#nodejs-mysql-cloudformation",
-    subtitle:
-      "An example for deploying a NodeJS app to Beanstalk using AWS CloudFormation.",
-    period: "November 2020",
-    tech: "AWS CloudFormation",
-    description: (
-      <>
-        <p>
-          An example of how you can perform Infrastructure-As-Code (IaC) using
-          AWS CloudFormation and Continuous-Integration/Continuous-Deployment
-          (CI/CD) using AWS CodePipeline.
-        </p>
-        <p>
-          This example deploys the NodeJS-ExpressJS-MySQL
-          Create-Read-Update-Delete (CRUD) application at{" "}
-          <a href="https://github.com/DigiPie/nodejs-mysql-aws-beanstalk">
-            DigiPie/nodejs-mysql-aws-beanstalk
-          </a>
-          , by using the CloudFormation IaC templates in this repository and
-          setting up a simple AWS CodePipeline.
-        </p>
-        <p>
-          I worked on this project to learn more about the{" "}
-          <a href="https://www.hashicorp.com/resources/getting-started-with-infrastructure-as-code-iac">
-            Infrastructure-as-Code
-          </a>{" "}
-          concept and{" "}
-          <a href="https://aws.amazon.com/cloudformation/">
-            AWS CloudFormation
-          </a>
-          .
-        </p>
-      </>
-    ),
-    links: [
-      {
-        name: "GitHub",
-        link: "https://github.com/DigiPie/nodejs-mysql-cloudformation",
-      },
-    ],
-   },
-  }
+const projects = []
 
 export default projects;
